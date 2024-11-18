15d5bd6a69953c9afecb3f4580b496a3d0b01b1a
Aline Henriques <alinealbhenriques@gmail.com>
2024-11-18 20:52:25 -0300

    Update lixeira-automatica.ino

--- código-arduino/lixeira-automatica/lixeira-automatica.ino
+++ código-arduino/lixeira-automatica/lixeira-automatica.ino
@@ -14,19 +14,30 @@ long readUltrasonicDistance(int triggerPin, int echoPin) {
 }
 
 Servo servo_9;
+const int buzzerPin = 8;
 
 void setup() {
   servo_9.attach(9, 500, 2500);
+  pinMode(buzzerPin, OUTPUT); 
 }
 
 void loop() {
   distancia = 0.01723 * readUltrasonicDistance(7, 6);
-  servo_9.write(0);
+  servo_9.write(0); 
   delay(300);
 
   while (distancia <= 20) {
     servo_9.write(180);
-    delay(2000);
+    playThankYouSound(); 
+    delay(2000); 
     distancia = 0.01723 * readUltrasonicDistance(7, 6);
   }
+}
+
+void playThankYouSound() {
+  tone(buzzerPin, 1000, 200); // Frequência em Hz, duração em ms
+  delay(250);
+  tone(buzzerPin, 1200, 200);
+  delay(250);
+  tone(buzzerPin, 1500, 300);
 }
